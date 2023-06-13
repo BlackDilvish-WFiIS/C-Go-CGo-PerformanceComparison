cdd0b20b6be8de03b98111b2f0ed4173a904c99d
BlackDilvish <gabriel.naleznik@gmail.com>
2023-06-13 22:03:14 +0200

    Change encoding to LF

--- 1.HelloWorld/C/run.sh
+++ 1.HelloWorld/C/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "Exercise 1 - C"
+echo "Exercise 1 - C (Optimization 0)"
 cd "C"
 gcc -g -O0 -o main_O0 main.c
 objdump -d main_O0 > disassembly_O0.txt
 wc -l disassembly_O0.txt
 time ./main_O0 > /dev/null
 
+echo "Exercise 1 - C (Optimization 3)"
 gcc -g -O3 -o main_O3 main.c
 objdump -d main_O3 > disassembly_O3.txt
 wc -l disassembly_O3.txt
--- 8.LongestCommonSubstring/C/run.sh
+++ 8.LongestCommonSubstring/C/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "Exercise 8 - C"
+echo "Exercise 8 - C (Optimization 0)"
 cd "C"
 gcc -g -O0 -o main_O0 main.c
 objdump -d main_O0 > disassembly_O0.txt
 wc -l disassembly_O0.txt
 time ./main_O0
 
+echo "Exercise 8 - C (Optimization 3)"
 gcc -g -O3 -o main_O3 main.c
 objdump -d main_O3 > disassembly_O3.txt
 wc -l disassembly_O3.txt
